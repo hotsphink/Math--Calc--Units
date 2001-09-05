4da5e5ee2527376059a80c989f94c8bf781b91bd
Steve Fink <sphink@gmail.com>
2001-09-05 20:21:18 +0000

    Cached name_to_canonical, which doubled the performance.

--- Math/Calc/Units/Convert/Multi.pm
+++ Math/Calc/Units/Convert/Multi.pm
@@ -50,7 +50,16 @@ sub to_canonical {
 
 # name_to_canonical : unitName -> value x baseUnit
 #
+# Memoizing this doubles the speed of the test suite.
+#
+my %CANON_CACHE;
 sub name_to_canonical {
+    my $unitName = shift;
+    $CANON_CACHE{$unitName} ||= [ _name_to_canonical($unitName) ];
+    return @{ $CANON_CACHE{$unitName} };
+}
+
+sub _name_to_canonical {
     my ($unitName) = @_;
     
     # First, check for compound units
@@ -66,8 +75,6 @@ sub name_to_canonical {
     return Math::Calc::Units::Convert::Base->to_canonical($unitName);
 }
 
-# get_class : unitName -> "Math::Calc::Units::Convert::<unitClass>"
-#
 sub get_class {
     my ($unitName) = @_;
     my (undef, $canon) = name_to_canonical($unitName);
